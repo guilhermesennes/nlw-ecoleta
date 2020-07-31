e43680f78b0aaad4c3152f117d0f610a648c94f2
Guilherme Sennes <guilherme.sennes@yahoo.com.br>
2020-07-31 17:33:21 -0300

    Next Level Week - Ecoleta

---
+++ src/database/db.js
@@ -0,0 +1,67 @@
+// importar a dependência do sqlite3
+
+const sqlite3 = require("sqlite3").verbose();
+
+// criar o objeto que irá fazer operações no banco de dados
+
+const db = new sqlite3.Database("./src/database/database.db");
+
+// utilizar o objeto de banco de dados para nossas operações
+db.serialize(() => {
+  
+  // com comandos SQL eu vou:
+
+
+  // 1- criar uma tabela
+  db.run(`
+    CREATE TABLE IF NOT EXISTS places (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      image TEXT,
+      name TEXT,
+      address TEXT,
+      address2 TEXT,
+      state TEXT,
+      city TEXT,
+      items TEXT
+    );
+  `)
+
+  // 2- inserir dados na tabela
+  const query = `
+    INSERT INTO places (
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+    ) VALUES (?,?,?,?,?,?,?);
+  `
+  
+  const values = [
+    "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    "Colectoria",
+    "Guilherme Gemballa, Jardim América",
+    "Número 260",
+    "Santa Catarina",
+    "Rio do Sul",
+    "Resíduos Eletrônicos, Lâmpadas"
+  ]
+
+  function afterInsertData (err) {
+    if(err) {
+      return console.log(err)
+    }
+
+    console.log("Cadastrado com sucesso");
+    console.log(this);
+  }
+
+  db.run(query, values, afterInsertData)
+
+  // 3- consultar os dados da tabela
+  //db.all(`SELECT * FROM places`, function(err, rows){})
+
+  // 4- deletar um dado da tabela
+});
